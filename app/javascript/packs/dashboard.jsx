f03dc34e129f1504455a95295636abfb4b56d1ae
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-26 11:50:07 +0100

    Add link to login

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -5,6 +5,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 class Dashboard extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      user: {},
+      loggedIn: false
+    }
+  }
+
+  componentDidMount () {
+    if(window.location.hash) {
+      let fitbitToken = window.location.hash.slice(1).split("&")[0].replace("access_token=", "")
+      console.log(fitbitToken);
+      this.setState({loggedIn: true})
+    }
   }
 
   render () {
@@ -14,6 +26,13 @@ class Dashboard extends Component {
           <h1 className="page-header">ReactFit</h1>
           <p className="lead">Your personal fitness dashboard</p>
         </header>
+        {!this.state.loggedIn &&
+          <div className="text-center">
+            <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DCXM&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
+              Log in with fitbit
+            </a>
+          </div>
+        }
       </div>
     )
   }
